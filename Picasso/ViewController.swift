e0a66484086ccfd531574e5c67c89c73064fa729
Valentina Ferrigno <ferrigno.valentina@icloud.com>
2018-10-19 12:24:27 +0200

    Initial Commit

---
+++ Picasso/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picasso
+//
+//  Created by Valentina Ferrigno on 19/10/2018.
+//  Copyright © 2018 EsauriTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PicassoTests/PicassoTests.swift
@@ -0,0 +1,34 @@
+//
+//  PicassoTests.swift
+//  PicassoTests
+//
+//  Created by Valentina Ferrigno on 19/10/2018.
+//  Copyright © 2018 EsauriTeam. All rights reserved.
+//
+
+import XCTest
+@testable import Picasso
+
+class PicassoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
